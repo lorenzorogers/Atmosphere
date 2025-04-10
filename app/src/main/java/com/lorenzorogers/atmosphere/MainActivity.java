e81ede99bdf75f8357c61c747f247182eb16a23d
Lorenzo Rogers <129799525+lorenzorogers@users.noreply.github.com>
2025-04-10 15:23:54 -0700

    fix: All the things

--- app/src/main/java/com/lorenzorogers/atmosphere/MainActivity.java
+++ app/src/main/java/com/lorenzorogers/atmosphere/MainActivity.java
@@ -26,9 +26,9 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        testButton = findViewById(R.id.testButton);
-        testButton.setOnClickListener(
-                view -> System.out.println(RequestUtils.fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,rain,wind_speed_10m,visibility,apparent_temperature"))
-        );
+        //testButton = findViewById(R.id.testButton);
+        //testButton.setOnClickListener(
+        //        view -> System.out.println(RequestUtils.fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,rain,wind_speed_10m,visibility,apparent_temperature"))
+       // );
     }
 }
