3bdc216c071c7ae43c97886de7265cdf8edfa296
Lorenzo Rogers <129799525+lorenzorogers@users.noreply.github.com>
2025-04-11 13:30:33 -0700

    fix: Font and fetch fix

--- app/src/main/java/com/lorenzorogers/atmosphere/MainActivity.java
+++ app/src/main/java/com/lorenzorogers/atmosphere/MainActivity.java
@@ -1,6 +1,7 @@
 package com.lorenzorogers.atmosphere;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -26,9 +27,14 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        //testButton = findViewById(R.id.testButton);
-        //testButton.setOnClickListener(
-        //        view -> System.out.println(RequestUtils.fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,rain,wind_speed_10m,visibility,apparent_temperature"))
-       // );
+        testButton = findViewById(R.id.testButton);
+        testButton.setOnClickListener(
+                view -> {
+                    RequestUtils.fetch(
+                            "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,rain,wind_speed_10m,visibility,apparent_temperature",
+                            e -> Log.println(Log.INFO, "Atmosphere", e)
+                    );
+                }
+        );
     }
 }
--- app/src/main/java/com/lorenzorogers/atmosphere/network/RequestUtils.java
+++ app/src/main/java/com/lorenzorogers/atmosphere/network/RequestUtils.java
@@ -1,39 +1,58 @@
 package com.lorenzorogers.atmosphere.network;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.function.Consumer;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
+
 // THIS MUST RUN ASYNC
 public class RequestUtils {
     public static final MediaType JSON = MediaType.get("application/json");
     static OkHttpClient client = new OkHttpClient();
 
-    public static String fetch(String url, Consumer<String> callback) {
+    public static void fetch(String url, Consumer<String> callback) {
         try {
-            return fetch(new URL(url));
+            fetch(new URL(url), callback);
         } catch (MalformedURLException e) {
-            return String.format("Failed to fetch URL %s", url);
+            System.out.printf("Failed to fetch URL %s", url);
         }
     }
-    public static String fetch(URL url) {
+
+    public static void fetch(URL url, Consumer<String> callback) {
         Request request = new Request.Builder()
                 .url(url)
                 .build();
 
-        try (Response response = client.newCall(request).execute()) {
-            if (!response.isSuccessful() || response.body() == null) {
-                throw new IOException(String.format("Unexpected error in API request to URL %s, %s", url, response));
+        Log.println(Log.DEBUG, "Atmosphere", String.format("Sending fetch to %s", url));
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
             }
 
-            return response.body().string();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                try (ResponseBody responseBody = response.body()) {
+                    if (!response.isSuccessful() || responseBody == null) {
+                        throw new IOException(String.format("Unexpected error in API request to URL %s, %s", url, responseBody));
+                    }
+
+                    callback.accept(responseBody.string());
+                }
+            }
+        });
     }
 }
