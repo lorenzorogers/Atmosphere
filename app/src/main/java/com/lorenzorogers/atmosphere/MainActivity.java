382692bde873655ad9a9e4aa4f1f3f80be3713a3
ForeverBoundless <michael.carpiano@smus.ca>
2025-04-23 18:53:50 -0700

    feat: back button actually works

--- app/src/main/java/com/lorenzorogers/atmosphere/MainActivity.java
+++ app/src/main/java/com/lorenzorogers/atmosphere/MainActivity.java
@@ -7,6 +7,9 @@ import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageView;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.lorenzorogers.atmosphere.forecast.LocationForecast;
@@ -18,14 +21,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        LocationForecast.get(48.42707924722373, -123.3645493928255, forecast -> {
+        // Back button navigates to HomeActivity
+        ImageView backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
 
+        LocationForecast.get(48.42707924722373, -123.3645493928255, forecast -> {
             TextView temperatureText = findViewById(R.id.temperatureText);
             TextView windSpeedText = findViewById(R.id.windSpeed);
             TextView visibilityText = findViewById(R.id.visibilityValue);
